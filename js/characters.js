95311e85037550e99eb0173e84c9602bf087228a
angiehelensanchez <angiehelensanchez@gmail.com>
2024-11-05 17:23:52 +0100

    style:Characters style

--- js/characters.js
+++ js/characters.js
@@ -16,7 +16,7 @@ async function fetchCharacterJson() {
 
 function createCharacterCard ({name, race, gender, ki, maxKi, affiliation, image}){
     return `
-        <div class="card mb-3" style="width: 30rem">
+        <div class="card mb-3" style="width: 300px">
             <div class="row g-0">
                 <div class="col-md-4">
                 <img src="${image}" class="img-fluid rounded-start" alt="Dragon ball character ${name}.">
@@ -25,11 +25,11 @@ function createCharacterCard ({name, race, gender, ki, maxKi, affiliation, image
                     <div class="card-body">
                         <h5 class="card-title">${name}</h5>
                         <p class="card-text">${race}-${gender}</p>
-                        <p class="card-subtitle">Base KI:</p>
+                        <p class="card-text2">Base KI:</p>
                         <p class="card-text">${ki}</p>
-                        <p class="card-subtitle">Total KI:</p>
+                        <p class="card-text2">Total KI:</p>
                         <p class="card-text">${maxKi}</p>
-                        <p class="card-subtitle">Afilliation:</p>
+                        <p class="card-text2">Afilliation:</p>
                         <p class="card-text">${affiliation}</p>
                     </div>
                 </div>
